b5b267d38fd0457deb4a2b34b5ecaff0210a3627
vagrant <vagrant@localhost.localdomain>
2013-03-31 14:23:36 +0200

    add cookbook "basic"

---
+++ site-cookbooks/basic/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: basic
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{zsh gcc make readline-devel}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
